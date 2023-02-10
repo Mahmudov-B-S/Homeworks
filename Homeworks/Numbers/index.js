f63c4a41d94ca84d96c7cf6549af8731a19548c5
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-10 11:36:19 +0500

    Numbers

---
+++ Homeworks/Numbers/index.js
@@ -0,0 +1,39 @@
+// Numbers
+
+//  1
+
+
+
+
+
+//   2.
+
+// let someNumbers = [12, 2, 32, 242, 23, 41, 32]
+// let max = Math.max(...someNumbers)
+// let min = Math.min(...someNumbers)
+// console.log(max - min);
+
+
+//   3. 
+
+// Trunc - sonning faqat butun qismini qaytaradi
+// console.log(7.7); // => 7
+// console.log(-9.5); // => -9
+
+// Floor - berilgan son yoki ozgaruvchini(taypi number bolgan) kichik butun qismini chiqaradi
+// console.log(7.9); //  => 7
+// console.log(-7.2); //  => -8
+
+// Ceil -  berilgan son yoki ozgaruvchini(taypi number bolgan) katta butun qismini chiqaradi
+// console.log(7.9); //  => 8
+// console.log(-7.2); //  => -7
+
+// parseInt - 
+// let a = "2"
+// let b = console.log(parseInt(a));
+// console.log(typeof(b));
+
+
+//   4. 
+
+// console.log(Math.trunc(Math.random()*100/2)+1);
