483c875719b7c80c6445ebeb11192aa571114830
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-13 22:03:39 +0500

    strings

---
+++ Homeworks/String/index.js
@@ -0,0 +1,23 @@
+// String
+
+// 1.  
+// let str1 = "salom"
+// let str2 = "lom"
+//     if (str1.localeCompare(str2) = 0) {
+//         onsole.log("bir biriga mos")
+//     } else if (str1.localeCompare(str2) > 0) {
+//         onsole.log("qisman mos")
+//     } else {
+//         console.log("bir biriga mos emas")
+//     }
+
+
+//  2. 
+
+    
+
+    // (str1.localCompare(str2) = 0) ? console.log("bir biriga mos")
+    // :
+    // (str1.localCompare(str2) > 0) ? console.log("qisman mos")
+    //     :
+    //     console.log("bir biriga mos emas")
