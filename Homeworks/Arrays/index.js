d42f9348082d940d4b183434f7c4d2b897dcfea9
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-22 23:11:01 +0500

    arrays

---
+++ Homeworks/Arrays/index.js
@@ -0,0 +1,16 @@
+
+
+//  1 
+// let ar = [1, 2, 3, 4, 5]
+// let res = ar.reduce((v, i) => v += i, 0)
+// console.log(res);
+
+
+//  2
+// let ar = ['webbrain', 'academy']
+
+// let res = ar.forEach((v, i)=> {
+//     console.log(v);
+//     return 1; 
+// })
+// console.log(res);
