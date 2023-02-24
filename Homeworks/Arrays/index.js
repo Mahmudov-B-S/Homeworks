d0e4ce1d2c45e2da869841e1854eabe1498c456a
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-24 13:23:44 +0500

    arrays

--- Homeworks/Arrays/index.js
+++ Homeworks/Arrays/index.js
@@ -12,7 +12,47 @@
 // let res = ar.forEach((v)=> {
 //     console.log(v);
 //     return ; 
-
-
 // })
 // console.log(res);
+
+
+//  3
+// ar1 = [ 1, {name: "wba"}]
+// ar2 = [ 1, {name: "wba"}]
+// console.log(ar1.toString() === ar2.toString());
+
+//  6
+
+// let users = [
+//     {id:1, year: 1998, engine: 1, name: "Tico"},
+//     {id:2, year: 2005, engine: 1.2, name: "Matiz"},
+//     {id:3, year: 2010, engine: 1.6, name: "Gentra"},
+//     {id:4, year: 2010, engine: 1.5, name: "Cobalt"},
+//     {id:5, year: 2012, engine: 2, name: "Malibu"},
+//     {id:6, year: 2000, engine: 1.2, name: "Damas"},
+//     {id:7, year: 2018, engine: 2.4, name: "Tracer"}
+// ]
+
+//1 let res = users.filter((v)=> v.year < 2000)
+// console.log(res);
+
+
+//2 let res = users.filter((v)=> v.year > 2010)
+// console.log(res);
+
+
+//3 let res = users.sort((a, b) => a.engine - b.engine )
+// console.log(res);
+
+
+//4 let res = users.sort((a, b) => a.year - b.year )
+// console.log(res);
+
+
+//5 let res = users.sort((a, b) => a.name.localeCompare(b.name) )
+// console.log(res);
+
+
+// let res = users.filter((v)=> v.year < 2000)
+// console.log(res);
+
