13d1b0e706a071137252fd78e5f6858a66824d3d
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-27 14:00:30 +0500

    m

--- Homeworks/CRUD/index.js
+++ Homeworks/CRUD/index.js
@@ -1,14 +1,38 @@
-let users = [
-    { id: 1, name: "Jasurbek Otelbauev" },
-    { id: 2, name: "Samariddin O'rinov" },
-    { id: 3, name: "Sardor Ibodov" },
-    { id: 4, name: "Samandar Ismoiljonov" },
-    { id: 5, name: "Ziyovuddinov Azizbek" }
-]
+// // Objects
 
-// Read --> filter, sort,
 
-const filter = (res) => {
-    let r = users.filter((vl) => vl.name.includes(res));
-    console.log(r);
-};
+// // let obj = { title: "webbarin", age: 2 }
+// // for(key in obj){
+// //     console.log(key);
+// // }
+
+
+// let obj = {
+//     title: "webbrain",
+//     age: 5,
+//     child: {
+//         age: 4,
+//         name: "webbrain",
+//         child: {
+//             age: 3
+//         }
+//     }
+// }
+// let sum = 0;
+//  while(obj){
+//    sum+=obj.age;
+//    obj=obj.child;
+//  }
+//  console.log(sum);
+
+
+// function lastNumber(arr) {
+//     while (arr.length > 1) {
+//         let array = arr.sort((a, b) => a - b);
+//         let res = array[array.length - 1] - array[array.length - 2];
+//         array.splice(array.length - 2, 2);
+//         res ? array.push(res) : null;
+//     }
+//     return arr;
+// }
+// console.log(lastNumber([11, 16, 12]));
